d9cb09a86b7307319b47fa692a204eb6bcc6b75d
KhaledNacef <khaledncf0@gmail.com>
2024-03-06 18:58:55 +0100

    done

--- finalproject/App.js
+++ finalproject/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import 'react-native-gesture-handler';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from './components/auth/login.jsx';
 import Signup from './components/auth/signup.jsx';
+import HomeScreen from './components/auth/home.jsx';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
+        
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
-
